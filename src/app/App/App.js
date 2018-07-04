c52fb7d9f1f61c2c40f879ddb15059cb074ee21a
Edmund Hung <edmund@edstud.io>
2018-07-04 17:02:34 +0800

    Introduce react-intl for i18n

--- src/app/App/App.js
+++ src/app/App/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
 import logo from './logo.svg';
 import './App.css';
+
+const messages = defineMessages({
+  title: {
+    id: 'app.title',
+    description: 'Title of the Application',
+    defaultMessage: 'Welcome to React',
+  },
+  introduction: {
+    id: 'app.introduction',
+    description: 'Description of the Application',
+    defaultMessage: 'To get started, edit {code} and save to reload.',
+  },
+});
 
 class App extends Component {
   render() {
@@ -8,10 +22,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            <FormattedMessage {...messages.title} />
+          </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <FormattedMessage {...messages.introduction} values={{ code: <code>src/App.js</code> }} />
         </p>
       </div>
     );
--- src/app/App/index.js
+++ src/app/App/index.js
@@ -1,3 +1,35 @@
+import React from 'react';
+import { IntlProvider } from 'react-intl';
 import App from './App';
 
-export default App;
+type Props = {
+  /**
+   * Locale of the UI
+   */
+  locale?: string,
+
+  /**
+   * Message Descriptor Map of the current locale
+   */
+  messages?: object,
+};
+class AppContainer extends React.PureComponent<Props> {
+  render() {
+    const {
+      props: { locale, messages },
+    } = this;
+
+    return (
+      <IntlProvider locale={locale} messages={messages}>
+        <App />
+      </IntlProvider>
+    );
+  }
+}
+
+AppContainer.defaultProps = {
+  locale: 'en',
+  messages: {},
+};
+
+export default AppContainer;
