3d27db0b355707897122ae576f8386fc9e78e9d4
David Van Isacker <davidvanisacker@gmail.com>
2018-07-04 23:52:32 +0900

    Reinstall storybook

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,2 @@
-import '@storybook/addon-options/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
-import '@storybook/addon-knobs/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,52 +1,7 @@
-import { configure, addDecorator } from '@storybook/react';
-import { setConsoleOptions, withConsole } from '@storybook/addon-console';
-import { setDefaults } from '@storybook/addon-info';
-import { setOptions } from '@storybook/addon-options';
-import { withStore } from './addon-store';
-
-const req = require.context('../src/components', true, /stories\.js$/)
+import { configure } from '@storybook/react';
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename))
+  require('../src/stories');
 }
 
-// @storybook/addon-console
-setConsoleOptions({
-  panelExclude: [],
-});
-
-// @storybook/addon-info
-setDefaults({
-  inline: true,
-  styles: {
-    infoBody: {
-      border: 'none',
-      boxShadow: 'none',
-    },
-    infoStory: {
-      textAlign: 'center',
-      border: '1px #eee dashed',
-      padding: '40px',
-      margin: '0 40px',
-    },
-    header: {
-      body: {
-        marginBottom: 0,
-      },
-    },
-  },
-});
-
-// @storybook/addon-options
-setOptions({
-  addonPanelInRight: true,
-});
-
-// Configure decorator
-addDecorator((storyFn, context) => withConsole()(storyFn)(context));
-
-// Configure Redux Store
-addDecorator(withStore);
-
-// Bootstrap
 configure(loadStories, module);
