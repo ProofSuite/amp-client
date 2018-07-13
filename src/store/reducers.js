7fd349065461074999e7df38cfe8f77e0d9ebc0c
David Van Isacker <davidvanisacker@gmail.com>
2018-07-13 16:58:06 +0900

    updated reducers and types with new domains/models

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,12 +1,18 @@
 // @flow
 import createReducer from './createReducer';
 import etherBalanceActionTypes from './actions/etherBalance';
+import accountBalancesActionTypes from './actions/accountBalances';
 import providerActionTypes from './actions/provider';
 import etherTxActionTypes from './actions/etherTx';
+import tokensActionTypes from './actions/tokens';
+import accountActionTypes from './actions/account';
 
 import * as etherBalanceEvents from './domains/etherBalance';
+import * as accountBalancesEvents from './domains/accountBalances';
 import * as providerEvents from './domains/provider';
 import * as etherTxEvents from './domains/etherTx';
+import * as tokensEvents from './domains/tokens';
+import * as accountEvents from './domains/account';
 
 export const etherBalance = createReducer(action => {
   const { type, payload } = action;
@@ -20,6 +26,24 @@ export const etherBalance = createReducer(action => {
       return etherBalanceEvents.updated(payload.address, payload.balance);
     default:
       return etherBalanceEvents.initialized();
+  }
+});
+
+export const accountBalances = createReducer(action => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case accountBalancesActionTypes.subscribeBalance:
+      return accountBalancesEvents.subscribed(payload.symbol);
+    case accountBalancesActionTypes.unsubscribeBalance:
+      return accountBalancesEvents.unsubscribed(payload.symbol);
+    case accountBalancesActionTypes.updateBalance:
+      console.log('updated', payload.symbol, payload.balance);
+      return accountBalancesEvents.updated(payload.symbol, payload.balance);
+    case accountBalancesActionTypes.clearBalances:
+      return accountBalancesEvents.cleared();
+    default:
+      return accountBalancesEvents.initialized();
   }
 });
 
@@ -54,5 +78,29 @@ export const etherTx = createReducer(action => {
       return etherTxEvents.etherTxConfirmed(payload.receipt);
     default:
       return etherTxEvents.initialized();
+  }
+});
+
+export const tokens = createReducer(action => {
+  const { type, payload } = action;
+  switch (type) {
+    case tokensActionTypes.updateToken:
+      return tokensEvents.tokenUpdated(payload.symbol, payload.address);
+    case tokensActionTypes.removeToken:
+      return tokensEvents.tokenRemoved(payload.symbol);
+    default:
+      return tokensEvents.initialized();
+  }
+});
+
+export const account = createReducer(action => {
+  const { type, payload } = action;
+  switch (type) {
+    case accountActionTypes.updateAccount:
+      return accountEvents.accountUpdated(payload.address);
+    case providerActionTypes.setProvider:
+      return accountEvents.accountUpdated(payload.address);
+    default:
+      return accountEvents.initialized();
   }
 });
--- src/types/index.js
+++ src/types/index.js
@@ -2,17 +2,33 @@
 import type { Store as ReduxStore, Dispatch as ReduxDispatch } from 'redux';
 import type { ProviderState, ProviderAction, ProviderEvent } from './provider';
 import type { EtherBalanceState, EtherBalanceAction, EtherBalanceEvent } from './etherBalance';
+import type { AccountBalancesState, AccountBalancesEvent, AccountBalancesAction } from './accountBalances';
 import type { EtherTxState, EtherTxAction, EtherTxEvent } from './etherTx';
+import type { TokenState, TokenAction, TokenEvent } from './tokens';
+import type { AccountState, AccountAction, AccountEvent } from './account';
 
 export type ReduxInitAction = { type: '@@INIT' };
-export type Action = ReduxInitAction | ProviderAction | EtherBalanceAction | EtherTxAction;
-export type Event = ProviderEvent | EtherBalanceEvent | EtherTxEvent;
+
+export type Action =
+  | ReduxInitAction
+  | ProviderAction
+  | EtherBalanceAction
+  | EtherTxAction
+  | TokenAction
+  | AccountBalancesAction
+  | AccountAction;
+
+export type Event = ProviderEvent | EtherBalanceEvent | EtherTxEvent | TokenEvent | AccountBalancesEvent | AccountEvent;
+
 export type ActionHandler = Action => Event;
 
 export type State = {
   provider: ProviderState,
   etherBalance: EtherBalanceState,
+  accountBalances: AccountBalancesState,
   etherTx: EtherTxState,
+  tokens: TokenState,
+  account: AccountState,
 };
 
 export type Store = ReduxStore<State, Action>;
