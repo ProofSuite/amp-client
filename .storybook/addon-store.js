3ad5ef124bd6921d8dabe43294a71e201f561f0a
David Van Isacker <davidvanisacker@gmail.com>
2018-07-13 16:47:19 +0900

    added initial account configuration on storybook store

--- .storybook/addon-store.js
+++ .storybook/addon-store.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from '../src/store';
+import { createProvider } from '../src/store/services/provider'
 
 export function withStore(storyFn) {
-  const store = createStore();
+
+  const store = createStore({
+    account: {
+      address: '0xe8e84ee367bc63ddb38d3d01bccef106c194dc47'
+    }
+  });
 
   return (
     <Provider store={store}>
