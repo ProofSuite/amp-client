b740e96e958b9ebfa93c141b0872accd57fe7026
Hassan Khokhar <tauqeerhassan66@gmail.com>
2018-07-16 16:49:44 +0500

    added <DepthChart />

---
+++ src/components/DepthChart/README.md
@@ -0,0 +1,17 @@
+```js
+import DepthChartRenderer from '../../components/DepthChartRenderer';
+```
+
+#### Properties
+* `data` - Data for Chart
+* `loading` - Loading status of chart's data
+* `title` - Title of Chart
+
+#### Example
+```js
+<DepthChartRenderer
+    data={data}
+    loading={false}
+    title="Price (BTC/USDT)"
+/>
+```
---
+++ src/components/DepthChart/stories.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, withKnobs } from '@storybook/addon-knobs/react';
+import { withInfo } from '@storybook/addon-info';
+import DepthChartRenderer from './index';
+import * as README from './README.md';
+import * as bidAsk from '../../jsons/bidAsk.json';
+
+storiesOf('DepthChart', module)
+  .addDecorator(withKnobs)
+  .add(
+    'Logged Out state',
+    withInfo({ text: README, source: false })(() => (
+      <DepthChartRenderer data={bidAsk.list} loading={false} title="Price (BTC/USDT)" />
+    ))
+  )
+  .add(
+    'Logged In state Buy Eth Form',
+    withInfo({ text: README, source: false })(() => (
+      <DepthChartRenderer data={bidAsk.list} loading={false} title="Price (BTC/USDT)" />
+    ))
+  )
+  .add(
+    'Logged In state Sell Eth Form',
+    withInfo({ text: README, source: false })(() => (
+      <DepthChartRenderer data={bidAsk.list} loading={false} title="Price (BTC/USDT)" />
+    ))
+  );
