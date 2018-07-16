9351f9445f14a6b6c766813a350e854b8ee1fd69
David Van Isacker <davidvanisacker@gmail.com>
2018-07-17 00:33:34 +0900

    fixed contract configuration

--- src/config/contracts.js
+++ src/config/contracts.js
@@ -2,16 +2,18 @@ import { Exchange, WETH } from 'proof-contracts-interfaces';
 
 export const EXCHANGE_ADDRESS = {
   '8888': Exchange.networks[8888].address,
-  '1': Exchange.networks[1].address,
-  '2': Exchange.networks[2].address,
-  '3': Exchange.networks[3].address,
-  '4': Exchange.networks[4].address,
+  '1000': Exchange.networks[1000].address,
+  '1': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '2': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '3': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '4': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
 };
 
 export const WETH_ADDRESS = {
-  '8888': WETH.network[8888].address,
-  '1': WETH.networks[1].address,
-  '2': WETH.networks[2].address,
-  '3': WETH.networks[3].address,
-  '4': WETH.networks[4].address,
+  '8888': WETH.networks[8888].address,
+  '1000': WETH.networks[1000].address,
+  '1': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '2': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '3': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
+  '4': '0x7cc4b1851c35959d34e635a470f6b5c43ba3c9c9',
 };
