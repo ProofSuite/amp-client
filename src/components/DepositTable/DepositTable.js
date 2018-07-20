391028e229cb692a1fc3a8f94b3e36d6c9ef4b25
David Van Isacker <davidvanisacker@gmail.com>
2018-07-20 17:33:31 +0900

    added deposit table

---
+++ src/components/DepositTable/DepositTable.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import DepositTableRenderer from './DepositTableRenderer';
+
+import styled from 'styled-components';
+
+type Props = {
+  depositData: Array<Object>,
+};
+
+class DepositTable extends React.PureComponent<Props, State> {
+  handleAllowance = () => {
+    console.log('Log allowance');
+  };
+
+  handleDeposit = () => {
+    console.log('Log deposit');
+  };
+
+  handleWithdraw = () => {
+    console.log('Log Withdraw');
+  };
+
+  render() {
+    const { depositData } = this.props;
+
+    return (
+      <DepositTableRenderer
+        depositData={depositData}
+        handleAllowance={this.handleAllowance}
+        handleDeposit={this.handleDeposit}
+        handleWithdraw={this.handleWithdraw}
+      />
+    );
+  }
+}
+
+export default DepositTable;
