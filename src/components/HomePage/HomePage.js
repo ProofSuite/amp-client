bcc00e2556651aa6c838da32b107735cb12501ad
Hassan Khokhar <tauqeerhassan66@gmail.com>
2018-07-20 07:24:37 +0500

    removed Useless types

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,27 +4,14 @@ import styled from 'styled-components';
 import type { LoadDataParams } from '../../types/homePage';
 
 type Props = {
-  loading: boolean,
-  decimals: number,
-  quoteToken: string,
-  baseToken: string,
-  sellOrderList: Array<Object>,
-  buyOrderList: Array<Object>,
-  ohlcvData: Array<Object>,
-  orderHistory: Array<Object>,
-  tradeHistory: Array<Object>,
   loadData: LoadDataParams => void,
 };
 
 export default class HomePage extends React.PureComponent<Props> {
-  static defaultProps = {
-    decimals: 2,
-  };
   componentDidMount() {
     this.props.loadData({ tokenId: 'token_id' });
   }
   render() {
-    const { buyOrderList, sellOrderList, loading, baseToken, quoteToken, decimals } = this.props;
     return <Row />;
   }
 }
