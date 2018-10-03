afff8129a646614305c64daf6fc17ef822e44b89
David Van Isacker <davidvanisacker@gmail.com>
2018-10-03 15:14:49 +0900

    fixed hot reload

--- src/app/index.js
+++ src/app/index.js
@@ -11,24 +11,27 @@ import LogoutPage from './LogoutPage'
 import TradingPage from './TradingPage'
 
 import SocketManager from '../components/SocketManager'
+import { ConnectedRouter } from 'connected-react-router'
 
 import '../styles/css/index.css'
 
-function App(props) {
+const App = ({ history }) => {
   return (
-    <SocketManager>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/wallet" component={WalletPage} />
-          <Route path="/exchange" component={ExchangePage} />
-          <Route path="/settings" component={SettingsPage} />
-          <Route path="/logout" component={LogoutPage} />
-          <Route path="/trade" component={TradingPage} />
-        </Switch>
-      </Layout>
-    </SocketManager>
+    <ConnectedRouter history={history}>
+      <SocketManager>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/wallet" component={WalletPage} />
+            <Route path="/exchange" component={ExchangePage} />
+            <Route path="/settings" component={SettingsPage} />
+            <Route path="/logout" component={LogoutPage} />
+            <Route path="/trade" component={TradingPage} />
+          </Switch>
+        </Layout>
+      </SocketManager>
+    </ConnectedRouter>
   )
 }
 
