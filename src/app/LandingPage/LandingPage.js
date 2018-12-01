e18aec152c9edd59d5f1ba62da4d0b13b8ad6fb8
David Van Isacker <davidvanisacker@gmail.com>
2018-12-01 23:04:38 +0900

    changed iframe components

--- src/app/LandingPage/LandingPage.js
+++ src/app/LandingPage/LandingPage.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { NonIdealState } from '@blueprintjs/core';
+import Iframe from 'react-iframe'
 
 class LandingPage extends React.PureComponent {
 
-  iframe = () => {
-    return {
-      __html: '<iframe style="position:fixed; width:100%; height:100%; top:0px; left:0px; z-index:10000000; background-color:#000; "  frameBorder="0" src="https://www.proofsuite.com/ampmarket" ></iframe>'
-    }
-  }
+  // iframe = () => {
+  //   return {
+  //     __html: '<iframe style="position:fixed; width:100%; height:100%; top:0px; left:0px; z-index:10000000; background-color:#000; frameBorder="0"; src="https://www.proofsuite.com/ampmarket" ></iframe>'
+  //   }
+  // }
 
   render() {
     return (
-      <div>
-        <div dangerouslySetInnerHTML={ this.iframe() } />
-      </div>
+        <Iframe
+          url="https://www.proofsuite.com/ampmarket"
+          position="fixed"
+          width="100%"
+          height="100%"
+          top="0px"
+          left="0px"
+          z-index="10000000"
+          allowFullScreen
+          styles={{backgroundColor: "#000"}}
+       />
     );
   }
 }
